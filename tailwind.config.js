56bc23b764e2b51bea7e7d75a4f0ebf5e6f0a386
Pandorelli <j167511@tafe.wa.edu.au>
2024-06-08 07:36:47 +0800

    feat: added colour classes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gunmetal": "#261F30",
+        "ghost-white": "#ECEFFF",
+        "pale-purple": "#896EAC",
+        "water-blue": "#00A3FF",
+      },
+    },
   },
   plugins: [],
 }
